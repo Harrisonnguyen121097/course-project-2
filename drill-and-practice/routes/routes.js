d38685e745dd53ed304076171ee3414e75211865
nguyenh40 <hung.g.nguyen@aalto.fi>
2023-10-03 22:57:03 +0300

    Register and login

--- drill-and-practice/routes/routes.js
+++ drill-and-practice/routes/routes.js
@@ -3,6 +3,7 @@ import * as mainController from "./controllers/mainController.js";
 import * as topicController from "./controllers/topicController.js";
 import * as questionController from "./controllers/questionController.js";
 import * as optionController from "./controllers/optionController.js";
+import * as authController from "./controllers/authController.js";
 
 const router = new Router();
 
@@ -16,5 +17,9 @@ router.get("/topics/:id/questions/:qId", questionController.showQuestion);
 router.post("/topics/:tId/questions/:qId/delete", questionController.deleteQuestion);
 router.post("/topics/:id/questions/:qId/options", optionController.addOption);
 router.post("/topics/:tId/questions/:qId/options/:oId/delete", optionController.deleteOption);
+router.get("/auth/register", authController.showRegister);
+router.post("/auth/register", authController.registerUser);
+router.get("/auth/login", authController.showLogin);
+router.post("/auth/login", authController.loginUser);
 
 export { router };
